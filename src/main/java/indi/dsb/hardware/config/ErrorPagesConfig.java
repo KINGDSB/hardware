44900f7ff74c3632ba76b080ece20da80536b144
dsb <754739481@qq.com>
2018-04-18 18:13:11 +0800

    错误页面

---
+++ src/main/java/indi/dsb/hardware/config/ErrorPagesConfig.java
@@ -0,0 +1,29 @@
+package indi.dsb.hardware.config;
+
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.web.servlet.ErrorPage;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+
+/** 
+ * 错误页面的配置 
+ */  
+@Configuration  
+public class ErrorPagesConfig {  
+    @Bean   //此注解一定记住要加上，别忘记  
+    public EmbeddedServletContainerCustomizer containerCustomizer(){  
+        return new EmbeddedServletContainerCustomizer() {  
+            @Override  
+            public void customize(ConfigurableEmbeddedServletContainer container) {  
+                                                        //状态码               错误页面的存储路径  
+                ErrorPage errorPage400 = new ErrorPage(HttpStatus.BAD_REQUEST, "/error.jsp");  
+                ErrorPage errorPage404 = new ErrorPage(HttpStatus.NOT_FOUND, "/error.jsp");  
+                ErrorPage errorPage500 = new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/error.jsp");  
+                //...可自己一个一个的补全  
+                container.addErrorPages(errorPage400,errorPage404,errorPage500);  
+            }  
+        };  
+    }  
+}
--- src/main/webapp/error.jsp
+++ src/main/webapp/error.jsp
@@ -8,6 +8,8 @@
 <title>错误页面</title>
 </head>
 <body>
-<h1>出错了</h1>
+<h1>出错啦！</h1>
+错误信息:
+${exception.message}
 </body>
 </html>
