438d423794523f22ef283d41e5be417b64f9372b
dsb <754739481@qq.com>
2018-05-15 16:19:38 +0800

    ip 端口 存储地址啥的改成服务器的了

--- src/main/java/indi/dsb/hardware/common/ApiContant.java
+++ src/main/java/indi/dsb/hardware/common/ApiContant.java
@@ -11,11 +11,11 @@ public class ApiContant {
     /**
      * 文件存储根路径
      */
-    public static final String SAVE_FILE_PATH = "E://ProgramData//files//center//";
+    public static final String SAVE_FILE_PATH = "C://files//hardware//";
 
     /**
      * 文件读取根路径
      */
-    public static final String READ_FILE_PATH = "http://localhost:8082/files/center";
+    public static final String READ_FILE_PATH = "http://47.254.38.242:8082/files/hardware";
 
 }
